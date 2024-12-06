86043d1f4ac77abfd2b236352689ee8257d9d04f
tramvaysus <148719015+tramvaysus@users.noreply.github.com>
2024-12-06 21:53:10 +0300

    Update tests.h
    добавлены тесты из пункта 2.2

--- tests.h
+++ tests.h
@@ -20,3 +20,24 @@ bool body_contains_test3()
     Candle candle(100.0, 11.0, 9.0, 9.5);
     return (candle.body_contains(100.0) ? true : false);
 }
+
+//тест 1 -  contains
+bool contains_test1()
+{
+    Candle candle(10.0, 11.0, 9.0, 9.5);
+    return (candle.contains(11.0) ? true : false);
+}
+
+//тест 2 - contains 
+bool contains_test2()
+{
+    Candle candle(10.0, 11.0, 9.0, 9.5);
+    return (candle.contains(9.0) ? true : false);
+}
+
+//тест 3 - contains 
+bool contains_test3()
+{
+    Candle candle(10.0, 1100.0, 9.0, 9.5);
+    return (candle.contains(1100.0) ? true : false);
+}
